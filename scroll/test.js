8fcb57be1b0bd30ef5288ddb8c4a39d0c0cf3d0b
amen2020 <243943679@qq.com>
2017-01-25 21:13:30 +0800

    debug

--- scroll/test.js
+++ scroll/test.js
@@ -3,37 +3,22 @@
     var panelWidth = $('.panel').width(),
         panelHeight = $('.panel').height();
     $('.panel div[id^="panel-"]').css('width',panelWidth).css('height',panelHeight);
-    var scrollErrorFn = function() {
-        console.log('滚动失败了')
-    };
-    var scroll = new IScroll('.panel', {
+    var bookScroll = new IScroll('.panel-book', {
+        click: true, // 允许鼠标点击事件
+        scrollX: false,
+        scrollY: true, // 禁止纵向滚动
+        tap: true // 没有滚动时触发的事件
+    });
+    var panelScroll = new IScroll('.panel', {
         bounce: false, // 禁止到达容器边缘的反弹效果
         click: true, // 允许鼠标点击事件
-        freeScroll: true, // 允许二维同时滚动
+        scrollX: true,
         scrollY: false, // 禁止纵向滚动
-        tap: 'scrollErrorFn', // 没有滚动时触发的事件
-        hScrollbar:false,
-        vScrollbar:false,
-        vScroll:false,
-        snap:true,
-        probeType:3,
-        onBeforeScrollStart: function ( e ) {
-            if ( this.absDistX > (this.absDistY + 5 ) ) {
-                e.preventDefault();
-            }
-        },
-        //解决第一次无法滑动的问题
-        onTouchEnd: function () {
-            var self = this;
-            if (self.touchEndTimeId) {
-               clearTimeout(self.touchEndTimeId);
-            }
-            self.touchEndTimeId = setTimeout(function () {
-                self.absDistX = 0;
-                self.absDistY = 0;
-            }, 600);
-        }
+        tap: true, // 没有滚动时触发的事件
+        snap: true,
+        probeType: 3
     });
+
     $('.weui-tabbar__item').eq(0).addClass('weui-bar__item_on').siblings().removeClass('weui-bar__item_on');
     // 在页面内点击底部菜单时，才需要触发的操作
     $('.weui-tabbar__item').each(function(index) {
@@ -44,15 +29,15 @@
             switch(index) {
                 case 0:
                     console.log('book');
-
+                    panelScroll.scrollToElement('#panel-book');
                     break;
                 case 1:
                     console.log('activity');
-
+                    panelScroll.scrollToElement('#panel-activity');
                     break;
                 case 2:
                     console.log('personal');
-
+                    panelScroll.scrollToElement('#panel-personal');
                     break;
                 // 不设置default操作
             }
